8140dc01787207be9e60857c30053962e476b87c
Murad H Sarker <sarker24@gmail.com>
2022-05-27 19:52:39 +0200

    Fix loading problem

--- src/pages/Dashboard/Users.js
+++ src/pages/Dashboard/Users.js
@@ -1,9 +1,65 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { toast } from "react-toastify";
+import Loading from "../Shared/Loading";
 
 const Users = () => {
+  const {
+    isLoading,
+    error,
+    data: users,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:5000/user").then((res) => res.json())
+  );
+  if (isLoading) return <Loading />;
+  if (error) return "An error has occurred: " + error.message;
+
+  const makeAdmin = (user) => {
+    fetch(`http://localhost:5000/user/admin/${user}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    refetch();
+    toast.success("Successfully made an admin");
+  };
   return (
-    <div>
-      <h2>This is users page</h2>
+    <div class="overflow-x-auto">
+      <table class="table w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>User name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role !== "admin" && (
+                    <button
+                      onClick={() => makeAdmin(user.email)}
+                      className="btn btn-xs"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button className="btn btn-xs">Delete User</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
